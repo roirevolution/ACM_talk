4a4ffe23f4f0f482c9140903aae1fa81825622c3
Danny Rivers <drivers@roirevolution.com>
2018-12-26 12:27:39 -0500

    Adds an 'order saved' page.

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -4,8 +4,20 @@ import { Request, Response } from "express";
  * GET /order
  * start a new pizza order
  */
-export let newOrder = (req: Request, res: Response) => {
+export {
+  newOrder,
+  saveOrder
+};
+
+function newOrder (req: Request, res: Response) {
   res.render("new_order", {
     title: "New Order"
   });
-};
+}
+
+function saveOrder (req: Request, res: Response) {
+  // get the
+  res.render("order_saved", {
+    title: "Saved"
+  });
+}
