b9a2e36e34897273e6a56d6e55250d421d191eb0
Danny Rivers <drivers@roirevolution.com>
2019-01-11 10:31:35 -0500

    Adds name and address to orders.

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -16,16 +16,25 @@ function newOrder (req: Request, res: Response) {
 }
 
 function saveOrder (req: Request, res: Response) {
-  addToOrders(Object.keys(req.body));
+  const body = req.body;
+  addToOrders(toppingsFrom(body), body.name, body.address);
   res.render("order_saved", {
     title: "Saved",
     text: "there"
   });
 }
 
-function addToOrders(toppings: Array<string>, name = "bob") {
+function toppingsFrom(body: Object): Array<string> {
+  return Object.keys(body).filter((t) => validTopping(t)) ;
+}
+
+function validTopping(topping: string): boolean {
+  return topping != "name" && topping != "address";
+}
+
+function addToOrders(toppings: Array<string>, name = "Matt", address = "4401 Atlantic Ave") {
   const orderId = toppings.sort().join(",");
-  orders.set(orderId, name);
+  orders.set(orderId, {name, address});
   console.log(orders);
 }
 
